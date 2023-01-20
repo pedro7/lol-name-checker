8d9cae0465c7bfa9ae49f98ee864033910058d67
pedro7 <114467456+pedro7@users.noreply.github.com>
2023-01-20 15:55:14 -0300

    Add files via upload

--- checker.py
+++ checker.py
@@ -4,25 +4,13 @@ from re import search
 from requests import get
 from urllib.parse import quote
 
+
 class Checker:
     def __init__(self, server, key=None):
         platforms = {
-            'BR'  : 'br1' ,
-            'EUNE': 'eun1',
-            'EUW' : 'euw1',
-            'LAN' : 'la1' ,
-            'LAS' : 'la2' ,
-            'NA'  : 'na1' ,
-            'OCE' : 'oc1' ,
-            'RU'  : 'ru'  ,
-            'TR'  : 'tr1' ,
-            'JP'  : 'jp1' ,
-            'KR'  : 'kr'  ,
-            'PH'  : 'ph2' ,
-            'SG'  : 'sg2' ,
-            'TW'  : 'tw2' ,
-            'TH'  : 'th2' ,
-            'VN'  : 'vn2'
+            'BR': 'br1', 'EUNE': 'eun1', 'EUW': 'euw1', 'LAN': 'la1', 'LAS': 'la2', 'NA': 'na1', 'OCE': 'oc1',
+            'RU': 'ru', 'TR': 'tr1', 'JP': 'jp1', 'KR': 'kr', 'PH': 'ph2', 'SG': 'sg2', 'TW': 'tw2', 'TH': 'th2',
+            'VN': 'vn2'
         }
         self._platform = platforms[server.upper()]
         self._server = server
@@ -35,7 +23,10 @@ class Checker:
             return self._get_name_availability_without_key(name)
 
     def _get_name_availability_with_key(self, name):
-        summoner_data = get(f'https://{self._platform}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{quote(name)}?api_key={self._key}')
+        summoner_data = get(
+            f'https://{self._platform}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{quote(name)}?'
+            f'api_key={self._key}'
+        )
         summoner_data.raise_for_status()
         summoner_data = summoner_data.json()
         level = summoner_data['summonerLevel']
@@ -53,5 +44,11 @@ class Checker:
             raise ValueError
         last_game = last_game.group()[23:]
         cleanup_date = search('Cleanup date [^:]*: [^<]*', html).group().strip()[-11:]
-        months = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
-        return datetime(int(cleanup_date[7:11]), months[cleanup_date[3:6]], int(cleanup_date[:2]), int(last_game[:2]), int(last_game[3:5]), int(last_game[6:8]))
+        months = {
+            'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10,
+            'Nov': 11, 'Dec': 12
+        }
+        return datetime(
+            int(cleanup_date[7:11]), months[cleanup_date[3:6]], int(cleanup_date[:2]), int(last_game[:2]),
+            int(last_game[3:5]), int(last_game[6:8])
+        )
